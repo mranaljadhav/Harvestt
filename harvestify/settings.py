ec9267814aca0e5a5ce1d66dabf82dd6b2fa0897
Mranal Jadhav <39871514+mranaljadhav@users.noreply.github.com>
2021-07-28 18:20:26 +0530

    Update settings.py

--- harvestify/settings.py
+++ harvestify/settings.py
@@ -132,3 +132,19 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
