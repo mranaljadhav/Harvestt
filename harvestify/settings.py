6ef1060142f455fd56ac01d6fd08a72469325b48
Mranal Jadhav <39871514+mranaljadhav@users.noreply.github.com>
2021-07-28 18:58:12 +0530

    Update settings.py

--- harvestify/settings.py
+++ harvestify/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '@^$#&k*z@3oa@&k^83ce!@-=s7dixpg7*1!szzayrkl_=0een6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False  
+DEBUG = True  
 
-ALLOWED_HOSTS = ['harvestt.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
